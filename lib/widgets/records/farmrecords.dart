7fa1b9fd482161a02de89e5cef6f5ecae3a19a9f
pious <109950629+pious2847@users.noreply.github.com>
2024-06-05 10:03:54 +0000

    Changes to be committed: 	modified:   lib/widgets/records/farmrecords.dart 	modified:   lib/widgets/tabels/farmstabels.dart

--- lib/widgets/records/farmrecords.dart
+++ lib/widgets/records/farmrecords.dart
@@ -8,6 +8,7 @@ import 'package:agricare/screens/farms.dart';
 import 'package:agricare/utils/farm.dart';
 import 'package:agricare/widgets/tabels/farmstabels.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/services.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
@@ -35,73 +36,181 @@ class _GeneratePdfState extends State<GeneratePdf> {
 
   @override
   Widget build(BuildContext context) {
-    return ContentDialog(
-      constraints: BoxConstraints(
-        maxWidth: MediaQuery.of(context).size.width * 0.8,
-        minWidth: MediaQuery.of(context).size.width * 0.6,
-      ),
-      content: Column(
-        children: [
-          Column(
-            children: [
-              const SizedBox(
-                height: 25,
+    return SingleChildScrollView(
+      child: ContentDialog(
+        constraints: BoxConstraints(
+            maxWidth: MediaQuery.of(context).size.width * 0.8, minWidth: 700),
+        content: Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.6,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Text(
+                    'KAMBANG CO-OPERATIVE FOOD FARMING AND',
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 7,
+                  ),
+                  Text(
+                    'MARKETING SOCIETY LIMITED',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Image(image: AssetImage('assets/images/logo.jpeg')),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'FARM RECORDS',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
               ),
-              Text(
-                'KAMBANG CO-OPERATIVE FOOD FARMING AND',
-                style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold),
-              ),
-              Text(
-                'MARKETING SOCIETY LIMITED',
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              const Image(image: AssetImage('assets/images/logo.jpeg'), height: 100,),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'FARM RECORDS',
-                style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w400),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-
-            ],
-          )
-        
-        ],
-      
+            ),
+                     
+                     SizedBox(
+              width: MediaQuery.of(context).size.width,
+              // height: MediaQuery.of(context).size.height ,
+              child: farms.isNotEmpty
+                  ? SingleChildScrollView(
+                      child: Table(
+                        border: TableBorder.all(),
+                        columnWidths: const {
+                          0: FractionColumnWidth(0.4), // Name
+                          1: FractionColumnWidth(0.3), // Location
+                          2: FractionColumnWidth(0.3), // Farm Produce
+                        },
+                        children: [
+                          const TableRow(
+                            decoration: BoxDecoration(
+                              color: Color.fromARGB(225, 116, 101, 204),
+                            ),
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Name',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Location',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Farm Produce',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              
+                            ],
+                          ),
+                          ...farms.map(
+                            (farm) => TableRow(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(farm.name),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(farm.location),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(farm.farmproduce),
+                                ),
+                               
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  : const Center(
+                      child: Text('No farms added yet'),
+                    ),
+            ),
+          SizedBox(height: 30,),
+            Row(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.35,
+                  child: FilledButton(
+                    onPressed: () {
+                      generatePdf(farms);
+                    },
+                    child: Text('Print'),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.35,
+                  child: Button(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Cancel'),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
 
-  Future<void> generatePdf(Farm farms) async {
-    final pdf = pw.Document();
-    pdf.addPage(
-      pw.MultiPage(
-        pageFormat: PdfPageFormat.a4,
-        build: (context) {
-          return farmTabels(farms);
-        },
-      ),
-    );
+ Future<void> generatePdf(List<Farm> farms) async {
+  final pdf = pw.Document();
+  final img = await rootBundle.load('assets/images/logo.jpg');
+    final imageBytes = img.buffer.asUint8List();
+    pw.Image logo = pw.Image(pw.MemoryImage(imageBytes));
+  pdf.addPage(
+    pw.MultiPage(
+      pageFormat: PdfPageFormat.a4,
+      build: (context) {
+        return [farmTabels(farms, logo)];
+      },
+    ),
+  );
 
-    await Printing.sharePdf(
-        bytes: await pdf.save(),
-        filename: 'Kambang-Cooperative-Farm-Records.pdf');
-    ;
-  }
+  await Printing.sharePdf(
+    bytes: await pdf.save(),
+    filename: 'Kambang-Cooperative-Farm-Records1.pdf',
+  );
+}
 }
--- lib/widgets/tabels/farmstabels.dart
+++ lib/widgets/tabels/farmstabels.dart
@@ -1,49 +1,127 @@
 import 'package:agricare/models/farm.dart';
 import 'package:agricare/screens/farms.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+// import 'package:fluent_ui/fluent_ui.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
 
-farmTabels(farms) => pw.Padding(
-    padding: const pw.EdgeInsets.all(10.0),
-    child: pw.Column(
+farmTabels(List<Farm> farms, image) =>  pw.Column(
       children: [
         pw.Container(
           alignment: pw.Alignment.center,
           child: pw.Header(
-            child: pw.Text('Farm Records',
-                style: pw.TextStyle(font: pw.Font.helvetica())),
-            decoration: pw.BoxDecoration(
-              border: pw.Border.all(),
+            child: pw.SizedBox(
+              // width: pw.MediaQuery.of(context).size.width * 0.6,
+              child: pw.Column(
+                mainAxisAlignment: pw.MainAxisAlignment.center,
+                children: [
+                   pw.SizedBox(
+                    height: 25,
+                  ),
+                  pw.Text(
+                    'KAMBANG CO-OPERATIVE FOOD FARMING AND',
+                    style: pw.TextStyle(
+                        fontSize: 30,
+                        color: PdfColor.fromHex('#89b6ed'),
+                        fontWeight: pw.FontWeight.bold),
+                  ),
+                   pw.SizedBox(
+                    height: 7,
+                  ),
+                  pw.Text(
+                    'MARKETING SOCIETY LIMITED',
+                    style: pw.TextStyle(
+                        fontSize: 20,
+                        color: PdfColor.fromHex('#89b6ed'),
+                        fontWeight: pw.FontWeight.bold),
+                  ),
+                   pw.SizedBox(
+                    height: 15,
+                  ),
+                  pw.Container(
+   alignment: pw.Alignment.center,
+   height: 200,
+   child: image,
+),
+                   pw.SizedBox(
+                    height: 20,
+                  ),
+                  pw.Text(
+                    'FARM RECORDS',
+                    style: pw.TextStyle(
+                        fontSize: 14,
+                        color: PdfColor.fromHex('#89b6ed'),
+                        fontWeight: pw.FontWeight.bold),
+                  ),
+                  pw.SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
             ),
+              
           ),
         ),
         pw.SizedBox(height: 20),
-        pw.Table.fromTextArray(
-          border: null,
-          cellAlignment: pw.Alignment.centerLeft,
-          headerDecoration: pw.BoxDecoration(
-            border: pw.Border.all(),
-            color: PdfColors.grey300,
-          ),
-          headerHeight: 25,
-          cellHeight: 30,
-          cellAlignments: {
-            0: pw.Alignment.centerLeft,
-            1: pw.Alignment.centerLeft,
-            2: pw.Alignment.centerLeft,
-            3: pw.Alignment.center,
-          },
-          headers: ['Name', 'Location', 'Farm Produce', 'Actions'],
-          data: farms.map(
-                (farm) => [
-                  farm.name,
-                  farm.location,
-                  farm.farmproduce,
-                  '', // Empty cell for actions
+        pw.SizedBox(
+          child: pw.Table(
+          border: pw.TableBorder.all(),
+          children: [
+            pw.TableRow(
+              decoration: pw.BoxDecoration(),
+              children: [
+                pw.Padding(
+                  padding: pw.EdgeInsets.all(8.0),
+                  child: pw.Text(
+                    'Name',
+                    style: pw.TextStyle(
+                      fontWeight: pw.FontWeight.bold,
+                    ),
+                  ),
+                ),
+                pw.Padding(
+                  padding: pw.EdgeInsets.all(8.0),
+                  child: pw.Text(
+                    'Location',
+                    style: pw.TextStyle(
+                      fontWeight: pw.FontWeight.bold,
+                    ),
+                  ),
+                ),
+                pw.Padding(
+                  padding: pw.EdgeInsets.all(8.0),
+                  child: pw.Text(
+                    'Name',
+                    style: pw.TextStyle(
+                      fontWeight: pw.FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ...farms.map(
+              (farm) => pw.TableRow(
+                children: [
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(8.0),
+                    child: pw.Text(farm.name),
+                  ),
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(8.0),
+                    child: pw.Text(farm.location),
+                  ),
+                  pw.Padding(
+                    padding: const pw.EdgeInsets.all(8.0),
+                    child: pw.Text(farm.farmproduce),
+                  ),
                 ],
-              )
+              ),
+            ),
+          ],
         ),
+      
+        ),
+        
       ],
-    ));
+    );
